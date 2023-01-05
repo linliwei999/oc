e558eee552e195becee576de03bd6aac09575c2f
linlw <linliwei999@foxmail.com>
2023-01-05 15:05:34 +0800

    myCloudMusicApp 添加启动页

---
+++ MyCloudMusicApp/MyCloudMusic/MyCloudMusic/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  MyCloudMusic
+//
+//  Created by 林立伟 on 2023/1/5.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ MyCloudMusicApp/MyCloudMusic/MyCloudMusic/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyCloudMusic
+//
+//  Created by 林立伟 on 2023/1/5.
+//
+
+#import <UIKit/UIKit.h>
+
+/// 测试注释
+@interface ViewController : UIViewController
+
+
+@end
+
