62195fe9ab90b818a787b49eaa7298cddd8dd0ff
linlw <linliwei999@foxmail.com>
2023-01-13 11:20:36 +0800

    add 手动代码的方式添加一个按钮控件到视图

--- MyCloudMusicApp/MyCloudMusic/MyCloudMusic/Controller/LoginController.m
+++ MyCloudMusicApp/MyCloudMusic/MyCloudMusic/Controller/LoginController.m
@@ -8,6 +8,7 @@
 #import "LoginController.h"
 
 @interface LoginController ()
+@property (nonatomic, strong) UIButton *otherButton;
 
 @end
 
@@ -15,17 +16,39 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    //纯代码方式添加一个按钮，其他控件也类似
+    self.otherButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    
+    //设置标题
+    [self.otherButton setTitle:@"纯代码方式添加按钮" forState:UIControlStateNormal];
+    [self.otherButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [self.otherButton setTitleColor:[UIColor greenColor] forState:UIControlStateHighlighted];
+    
+    //设置点击事件
+    // UIControlEventTouchUpInside - 按下在抬起事件
+    [self.otherButton addTarget:self action:@selector(otherClcik:) forControlEvents:UIControlEventTouchUpInside ];
+    
+    self.otherButton.backgroundColor = [UIColor redColor];
+    self.otherButton.layer.cornerRadius = 5;
+    
+    //添加到容器
+    [self.view addSubview: self.otherButton];
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+// 布局完成后调用
+// 在这里才能获取到控件实际尺寸
+-(void)viewDidLayoutSubviews {
+    [super viewDidLayoutSubviews];
+    //设置按钮宽高
+    //在这里才能获取到控件真正布局后的尺寸
+    //当然还可以在代码中使用类似可视化中的那种约束
+    //只是在iOS很少在代码中直接用系统提供的约束
+    //而是使用第三方框架，因为用起来更方便
+    self.otherButton.frame = CGRectMake(16, 600, self.view.frame.size.width - 32, 41);
 }
-*/
+
+-(void)otherClcik:(UIButton *)sender {
+    NSLog(@"LoginController otherClcik");
+}
 
 @end
